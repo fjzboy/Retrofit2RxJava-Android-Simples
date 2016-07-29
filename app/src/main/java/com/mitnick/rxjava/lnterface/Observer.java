bc0167d0e8a5b623c396e6421e0d5201a07d5dcb
Chengzj <1102743539@qq.com>
2016-07-29 20:33:02 +0800

    add the observer pattern

--- app/src/main/java/com/mitnick/rxjava/lnterface/Observer.java
+++ app/src/main/java/com/mitnick/rxjava/lnterface/Observer.java
@@ -2,10 +2,13 @@ package com.mitnick.rxjava.lnterface;
 
 /**
  * Created by Michael Smith on 2016/7/24.
+ * 观察者
  */
 
-public interface Observer<T> {
-    public void register(T t);
+public interface Observer {
+    public void register(Observable observable);
 
-    public void unregister(T t);
+    public void unregister(Observable observable);
+
+    public void notifyData();
 }
